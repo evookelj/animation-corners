f75002a1ebe50e081bcdbd0e15557d7bce870d48
evookelj <evukelj@stuy.edu>
2017-02-17 17:18:49 -0500

    color+DVD pic

--- canvas.js
+++ canvas.js
@@ -6,7 +6,8 @@ ctx = c.getContext("2d");
 var rid;
 
 var circ = function() {
-    var x = 1;
+    var x = 0;
+    var dx = 1;
     window.cancelAnimationFrame( rid );
 
     var drawCirc = function() {
@@ -18,10 +19,12 @@ var circ = function() {
 	ctx.fillStyle = "#ffb6c1";
 	ctx.fill();
 
-	x++;
 	if (x==c.height/2) {
-	    x=1;
+	    dx = -1;
+	} else if (x==0) {
+	    dx = 1;
 	}
+	x += dx;
 	rid = window.requestAnimationFrame(drawCirc);
     };
 
@@ -29,10 +32,12 @@ var circ = function() {
 }
 
 var dvdIt = function() {
-    var x = Math.random()*c.width;
+    //random start + create starting velocity
+    var x = Math.random()*c.width; 
     var y = Math.random()*c.height;
     var xvol = 1;
     var yvol = 1;
+    var offset = 40;
     
     window.cancelAnimationFrame( rid );
 
@@ -40,14 +45,24 @@ var dvdIt = function() {
 	ctx.clearRect(0,0,c.width,c.height);
 	console.log(rid);
 
+	//draw new box
 	ctx.beginPath();
-	ctx.strokeRect(x,y,10,10);
+	//ctx.strokeRect(x,y,10,10);
+	pic = new Image();
+	pic.src = "pic.png"
+	ctx.drawImage(pic,x,y,offset,offset);
 	ctx.stroke();
 
-	if (x==c.width) { xvol = -1; }
-	if (y==c.height) { yvol = -1; }
-	if (x==0) { xvol = 1; }
-	if (y==0) { yvol = 1; }
+	//bounce on walls
+	if (x>=c.width-offset) { xvol = -1; }
+	if (y>=c.height-offset) { yvol = -1; }
+	if (x<=0) { xvol = 1; }
+	if (y<=0) { yvol = 1; }
+
+	//important moment notification
+	if ((x==0 || x==c.width-offset) && (y==0 || y==c.height-offset)) {
+	    window.alert("IT HIT THE CORNER. YOU WIN");
+	}
 	
 	x+= xvol;
 	y+= yvol;
